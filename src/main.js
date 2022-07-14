b9797e50cf82555f31e74a9cfa3e291de73ea6ea
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-14 16:21:56 +0800

    Refactor: Extract vue-router logic to its own file
    - Add new folder named router
    - Add index.js file that contains the logic

--- src/main.js
+++ src/main.js
@@ -1,25 +1,6 @@
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
+import router from "./router";
 import App from "./App.vue";
 import "./assets/main.css";
-
-import Home from "./views/Home.vue";
-import About from "./views/About.vue";
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "Home",
-      component: Home,
-    },
-    {
-      path: "/about",
-      name: "About",
-      component: About,
-    },
-  ],
-});
 
 createApp(App).use(router).mount("#app");
