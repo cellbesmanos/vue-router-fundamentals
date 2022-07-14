49b5c123b652c35caba30763cd5b8d869075b939
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-14 17:05:20 +0800

    Feat: Add new page views and establish routes
    - Brazil, Hawaii, Panama, Jamaica

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Hi, this is the Home view!</h1>
+  <div class="home">
+    <h1>All Destinations</h1>
+
+    <div class="destinations">
+      <router-link
+        v-for="destination of destinations"
+        :key="destination.id"
+        :to="destination.slug"
+        ><h2>
+          {{ destination.name }}
+          <img :src="`/images/${destination.image}`" :alt="destination.name" />
+        </h2>
+      </router-link>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import sourceData from "@/data.json";
+const destinations = sourceData.destinations;
+</script>
