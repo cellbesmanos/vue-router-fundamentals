933dc8d1f21082a03501fb61d9b2278d574f9ced
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-14 16:15:09 +0800

    Feat: Connect routes to App

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+
   <main>
-    <h1>Hello World!</h1>
+    <router-view></router-view>
   </main>
 </template>
