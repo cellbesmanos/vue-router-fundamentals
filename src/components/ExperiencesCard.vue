34d997ecc14ae77ac25bfdcb8348edce96829847
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-15 18:06:22 +0800

    Feat: Add ExperiencesCard component

---
+++ src/components/ExperiencesCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <img
+      :src="`/images/${props.experience.image}`"
+      :alt="props.experience.name"
+    />
+    <span class="card__text">
+      {{ props.experience.name }}
+    </span>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  experience: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -7,11 +7,24 @@
       <p>{{ destination.description }}</p>
     </div>
   </article>
+
+  <section class="experiences">
+    <h2>Top Experiences in {{ destination.name }}</h2>
+
+    <div class="cards">
+      <ExperiencesCard
+        v-for="experience in destination.experiences"
+        :key="experience.slug"
+        :experience="experience"
+      />
+    </div>
+  </section>
 </template>
 
 <script setup>
 import { computed } from "vue";
 import sourceData from "@/data.json";
+import ExperiencesCard from "../components/ExperiencesCard.vue";
 
 const props = defineProps({
   id: {
