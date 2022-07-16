f9c6047e40015fcbb30c3f8d6835f0af4562ac8a
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-16 11:24:49 +0800

    Feat: Add page route transition for DestinationShow

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,36 @@
   <TheNavigation />
 
   <main class="container">
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <transition name="slideUp" mode="out-in">
+        <component :is="Component" :key="route.path"></component>
+      </transition>
+    </router-view>
   </main>
 </template>
 
 <script setup>
+import { useRoute } from "vue-router";
 import TheNavigation from "./components/TheNavigation.vue";
+
+const route = useRoute();
 </script>
+
+<style>
+.slideUp-enter-from,
+.slideUp-leave-to {
+  opacity: 0;
+  transform: translateY(8%);
+}
+
+.slideUp-enter-to,
+.slideUp-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.slideUp-enter-active,
+.slideUp-leave-active {
+  transition: all 0.4s ease-out;
+}
+</style>
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,33 +1,30 @@
 <template>
-  <article class="destination">
-    <h1>{{ destination.name }}</h1>
-
-    <BackButton />
-
-    <div class="destination-details">
-      <img :src="`/images/${destination.image}`" :alt="destination.name" />
-      <p>{{ destination.description }}</p>
-    </div>
-  </article>
-
-  <section class="experiences">
-    <h2>Top Experiences in {{ destination.name }}</h2>
-
-    <div class="cards">
-      <router-link
-        v-for="experience in destination.experiences"
-        :key="experience.slug"
-        :to="{
-          name: 'experience.show',
-          params: { experienceSlug: experience.slug },
-        }"
-      >
-        <ExperiencesCard :experience="experience" />
-      </router-link>
-    </div>
-
-    <router-view />
-  </section>
+  <div>
+    <article class="destination">
+      <h1>{{ destination.name }}</h1>
+      <BackButton />
+      <div class="destination-details">
+        <img :src="`/images/${destination.image}`" :alt="destination.name" />
+        <p>{{ destination.description }}</p>
+      </div>
+    </article>
+    <section class="experiences">
+      <h2>Top Experiences in {{ destination.name }}</h2>
+      <div class="cards">
+        <router-link
+          v-for="experience in destination.experiences"
+          :key="experience.slug"
+          :to="{
+            name: 'experience.show',
+            params: { experienceSlug: experience.slug },
+          }"
+        >
+          <ExperiencesCard :experience="experience" />
+        </router-link>
+      </div>
+      <router-view />
+    </section>
+  </div>
 </template>
 
 <script setup>
