0f90ba2dc64d3a53a0b11c68a196f9ba69ee432e
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-07-16 13:25:37 +0800

    Fix: Add navigation guards for invalid parameters

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
+import sourceData from "@/data.json";
 
 const DestinationShow = () => import("@/views/DestinationShow.vue");
 const ExperienceShow = () => import("@/views/ExperienceShow.vue");
@@ -16,12 +17,42 @@ const routes = [
     name: "destination.show",
     component: DestinationShow,
     props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+    beforeEnter(to) {
+      const id = sourceData.destinations.find(
+        (destination) => destination.id === parseInt(to.params.id)
+      );
+
+      if (!id) {
+        return {
+          name: "NotFound",
+          params: { pathMatch: to.path.split("/").slice(1) },
+          query: to.query,
+          hash: to.hash,
+        };
+      }
+    },
     children: [
       {
         path: ":experienceSlug",
         name: "experience.show",
         component: ExperienceShow,
         props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+        beforeEnter(to) {
+          const experience = sourceData.destinations
+            .find((destination) => destination.id === parseInt(to.params.id))
+            .experiences.find(
+              (experience) => experience.slug === to.params.experienceSlug
+            );
+
+          if (!experience) {
+            return {
+              name: "NotFound",
+              params: { pathMatch: to.path.split("/").slice(1) },
+              query: to.query,
+              hash: to.hash,
+            };
+          }
+        },
       },
     ],
   },
